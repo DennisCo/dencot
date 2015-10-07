b8651c2f78ec230c79d8ef5e3d11db54b1e551e5
DennisCo <cotoman.den@gmail.com>
2015-10-07 10:46:41 +0300

    Create test.setub.php

---
+++ test/test.setub.php
@@ -0,0 +1,18 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Name=Test
+Description=Test module.
+Version=0.0.2
+Date=2015-08-03
+Author=Attar
+Copyright=Copyright (c) 2015, Become The Changes SRL (c) 2015, Become The Changes SRL
+Notes=Private License
+Auth_guests=R
+Lock_guests=R
+Auth_members=R
+Lock_members=
+Recommends_modules=
+Recommends_plugins=
+[END_COT_EXT]
+==================== */
